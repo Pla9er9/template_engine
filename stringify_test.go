5af6fb2c59c5216b2f315edb9570debdd083d8bd
Pla9er <drugiekonto_99@o2.pl>
2024-07-03 14:59:50 +0200

    add new tests to stringify function

---
+++ stringify_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStringify(t *testing.T) {
+	testCases := []TestCase{
+		{
+			Input:          "aba",
+			ExpectedOutput: "aba",
+		},
+		{
+			Input:          1,
+			ExpectedOutput: "1",
+		},
+		{
+			Input:          true,
+			ExpectedOutput: "true",
+		},
+		{
+			Input:          []string{"item1", "item2"},
+			ExpectedOutput: "[\"item1\",\"item2\"]",
+		},
+		{
+			Input: map[string]int{
+				"water": 2,
+				"oil":   3,
+				"milk":  4,
+			},
+			ExpectedOutput: "{\"milk\":4,\"oil\":3,\"water\":2}",
+		},
+	}
+
+	for _, test := range testCases {
+		str := Stringify(test.Input)
+
+		if str != test.ExpectedOutput {
+			errorMsg := fmt.Sprintf(
+				"Stringify gave wrong output\nExpected output: \n%v \nFinal output: \n%v",
+				test.ExpectedOutput,
+				str,
+			)
+			t.Error(errorMsg)
+		}
+	}
+}
